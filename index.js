efcd3f660ac9517ff03ad02aca8cb97fa86d30dd
Seer <kmkevins97@gmail.com>
2023-04-19 17:07:55 -0700

    Added questions based on acceptance criteria

--- index.js
+++ index.js
@@ -2,7 +2,17 @@
 const inquirer = require("inquirer");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    "What is the title of this project?",
+    "Briefly describe this project.",
+    "What are the installation instructions?",
+    "How does a user use this application?",
+    "What are the contribution guidelines for this project?",
+    "List any tests for this project.",
+    "What kind of license does this project use?",
+    "What is your GitHub user name?",
+    "What is your email?",
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
